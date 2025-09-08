699c4c4ab564b8fb1150c65069591415157a93eb
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2025-09-08 20:26:55 -0300

    Modularização da main

--- main.py
+++ main.py
@@ -23,6 +23,38 @@ def draw_cost_generation(y_list1, y_list2, y_list3, y_list4):
 
     plt.show()
 
+def draw_final_cost(coords, best_route_guloso, best_route_ts, best_route_ag):
+    fig = plt.figure(figsize=(15, 5))
+    ax1 = fig.add_subplot(141)
+    ax2 = fig.add_subplot(142)
+    ax3 = fig.add_subplot(143)
+    ax4 = fig.add_subplot(144)
+    # Desenha o caminho entre os pontos em sequência
+    for primeiro, segundo in zip(coords[:-1], coords[1:]):
+        ax1.plot([primeiro.x, segundo.x], [primeiro.y, segundo.y], 'b')
+    ax1.plot([coords[0].x, coords[-1].x], [coords[0].y, coords[-1].y], 'b')
+    # Desenha os pontos
+    for c in coords:
+        ax1.plot(c.x, c.y, 'ro')
+    for primeiro, segundo in zip(best_route_guloso[:-1], best_route_guloso[1:]):
+        ax2.plot([primeiro.x, segundo.x], [primeiro.y, segundo.y], 'b')
+    ax2.plot([best_route_guloso[0].x, best_route_guloso[-1].x], [best_route_guloso[0].y, best_route_guloso[-1].y], 'b')
+    for c in best_route_guloso:
+        ax2.plot(c.x, c.y, 'ro')
+    for primeiro, segundo in zip(best_route_ts[:-1], best_route_ts[1:]):
+        ax3.plot([primeiro.x, segundo.x], [primeiro.y, segundo.y], 'b')
+    ax3.plot([best_route_ts[0].x, best_route_ts[-1].x], [best_route_ts[0].y, best_route_ts[-1].y], 'b')
+    for c in best_route_ts:
+        ax3.plot(c.x, c.y, 'ro')
+    for primeiro, segundo in zip(best_route_ag[:-1], best_route_ag[1:]):
+        ax4.plot([primeiro.x, segundo.x], [primeiro.y, segundo.y], 'b')
+    ax4.plot([best_route_ag[0].x, best_route_ag[-1].x], [best_route_ag[0].y, best_route_ag[-1].y], 'b')
+    for c in best_route_ag:
+        ax4.plot(c.x, c.y, 'ro')
+
+    plt.tight_layout()
+    plt.show()
+
 if __name__ == "__main__":
     coords = []
     for i in range(10):
@@ -31,17 +63,6 @@ if __name__ == "__main__":
         )
 
 
-    fig = plt.figure(figsize=(15, 5))
-    ax1 = fig.add_subplot(131)
-    ax2 = fig.add_subplot(132)
-    ax3 = fig.add_subplot(133)
-    # Desenha o caminho entre os pontos em sequência
-    for primeiro, segundo in zip(coords[:-1], coords[1:]):
-        ax1.plot([primeiro.x, segundo.x], [primeiro.y, segundo.y], 'b')
-    ax1.plot([coords[0].x, coords[-1].x], [coords[0].y, coords[-1].y], 'b')
-    # Desenha os pontos
-    for c in coords:
-        ax1.plot(c.x, c.y, 'ro')
 
     algoritmo_genetico = AlgoritmoGenetico()
     tempera_simulada = TemperaSimulada(30, 0.99)
@@ -52,18 +73,9 @@ if __name__ == "__main__":
 
     print(f"Custo - Algoritmo Genetico: {best_distance_ag}")
     print(f"Custo - Tempera Simulada: {best_distance_ts}")
+    print(f"Custo - Escolha Gulosa: {best_distance_guloso}")
 
 
-    for primeiro, segundo in zip(best_route_ag[:-1], best_route_ag[1:]):
-        ax2.plot([primeiro.x, segundo.x], [primeiro.y, segundo.y], 'b')
-    ax2.plot([best_route_ag[0].x, best_route_ag[-1].x], [best_route_ag[0].y, best_route_ag[-1].y], 'b')
-    for c in best_route_ag:
-        ax2.plot(c.x, c.y, 'ro')
-    for primeiro, segundo in zip(best_route_ts[:-1], best_route_ts[1:]):
-        ax3.plot([primeiro.x, segundo.x], [primeiro.y, segundo.y], 'b')
-    ax3.plot([best_route_ts[0].x, best_route_ts[-1].x], [best_route_ts[0].y, best_route_ts[-1].y], 'b')
-    for c in best_route_ts:
-        ax3.plot(c.x, c.y, 'ro')
 
 
     dist_line_guloso = []
@@ -73,6 +85,6 @@ if __name__ == "__main__":
         dist_line_guloso.append(best_distance_guloso)
         dist_line_aleatorio.append(init_dist)
 
-    plt.tight_layout()
-    plt.show()
+
+    draw_final_cost(coords, best_route_guloso, best_route_ts, best_route_ag)
     draw_cost_generation(cost_time_ag, cost_time_ts, dist_line_guloso, dist_line_aleatorio)
